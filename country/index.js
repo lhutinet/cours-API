426514bb8adbe1370561707ea09a684f0e2d100a
laurent <lhutinet@free.fr>
2024-11-19 19:26:00 +0100

    Add files via upload

--- country/index.js
+++ country/index.js
@@ -1,5 +1,17 @@
 // 1 - Tester le lien de l'API dans le navigateur (https://restcountries.com/v3.1/all)
 
+let countries = [];
+async function fetchccountries() {
+  await fetch("https://restcountries.com/v3.1/0/")
+    .then((res) => res.json())
+    .then((data) => (countries = data));
+  // fetch("https://restcountries.com/v3.1/name/name")
+  //     .then((res) => res.json())
+  //     .then((data) => (countries = data.name));
+  console.log(countries);
+}
+
+fetchccountries();
 // 2 - Créer une fonction pour "fetcher" les données, afficher les données dans la console.
 
 // 3 - Passer les données à une variable
@@ -7,7 +19,7 @@
 // 4 - Créer une fonction d'affichage, et paramétrer l'affichage des cartes de chaque pays grace à la méthode MAP
 
 // 5 - Récupérer ce qui est tapé dans l'input et filtrer (avant le map) les données
-coutry.name.includes(inputSearch.value);
+// coutry.name.includes(inputSearch.value);
 
 // 6 - Avec la méthode Slice gérer le nombre de pays affichés (inputRange.value)
 
